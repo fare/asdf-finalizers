ee9cdafbbcd2261d31ee86ac3e1765f94a3842c7
Francois-Rene Rideau <tunes@google.com>
2012-06-29 03:27:41 -0400

    Warn when initially using list-of outside of finalized source code. Refactor a bit.

--- list-of.lisp
+++ list-of.lisp
@@ -12,23 +12,20 @@
     (let ((*package* (find-package :cl)))
       (intern (format nil "LIST-OF-~S-P" type) :list-of))))
 
-(defun is-list-of-type-p (x type)
-  (typecase x
-    (null
-     t)
-    (cons
-     (and (typep (car x) type)
-	  (is-list-of-type-p (cdr x) type)))
-    (t
-     nil)))
+(defun list-of-type-predicate (type)
+  #'(lambda (x)
+      (loop :for c = x :then (cdr c) :while (consp c) :always (typep (car c) type)
+	    :finally (return (null c)))))
 
 (defun ensure-list-of-predicate (type &optional predicate)
   (unless predicate
     (setf predicate (list-of-predicate-for type)))
   (check-type predicate symbol)
   (unless (fboundp predicate)
-    (setf (symbol-function predicate)
-	  #'(lambda (x) (is-list-of-type-p x type)))))
+    (unless (using-finalizers-p)
+      (warn "Defining list-of predicate ~S outside of a finalized Lisp source file" predicate))
+    (setf (symbol-function predicate) (list-of-type-predicate type)))
+  nil)
 
 (defmacro declare-list-of (type &optional predicate)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
