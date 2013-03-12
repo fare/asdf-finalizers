525f5beb9b21a83c84640b42a0b0e53df7ce402c
Francois-Rene Rideau <tunes@google.com>
2013-03-12 16:45:10 -0400

    Don't export symbol for functionality we don't provide (yet).

--- pkgdcl.lisp
+++ pkgdcl.lisp
@@ -14,7 +14,6 @@
    #:assert-no-finalizer-left-behind
    #:compile-check-finalizers
    #:check-finalizers-around-compile
-   #:finalized-cl-source-file
    #:call-with-finalizers
    #:with-finalizers
    #:using-finalizers-p
